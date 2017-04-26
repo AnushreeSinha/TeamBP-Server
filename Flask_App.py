0c670a96e627dd5dba796b2df10d7a68fd58ac9c
R. Dennis Steed <R. Dennis Steed>
2017-04-26 03:53:17 -0400

    Added entry point for doctor that fetches all vitals for current patient into list

--- Flask_App.py
+++ Flask_App.py
@@ -4,9 +4,36 @@ import logging
 from fhirclient import client
 from fhirclient.models.medication import Medication
 from fhirclient.models.medicationorder import MedicationOrder
+from fhirclient.models.observation import Observation
+import fhirclient.models.bundle as b
 import pprint
 
 from flask import Flask, request, redirect, session
+
+def iterentries(qry,smart):
+    """ Generator to provide the entries in a paged bundle. Hides the
+        process of pages being passed.
+        input:
+            qry is the portion of the REST URL after the server
+        yields:
+            a tuble containing
+            entry - a single entry from the bundle
+            blob - the most recent portion of the bundle downloaded
+        Note:  had trouble nesting loops, probably because of
+            a timeout between page fetches.
+    """
+    bund = b.Bundle.read_from(qry,smart.server)
+    have_page = bund.entry
+    while have_page:
+        for item in bund.entry:
+            yield item,bund
+        next_link = next((item.url for item in bund.link if item.relation == 'next'),None)
+        if next_link:
+            qry = next_link.rpartition('?')[2]
+            bund = b.Bundle.read_from('?'+qry,smart.server)
+        else:
+            have_page = False
+
 
 # app setup
 smart_defaults = {
@@ -22,8 +49,10 @@ app = Flask(__name__)
 def _save_state(state):
     session['state'] = state
 
-def _get_smart():
+def _get_smart(new_settings={}):
     state = session.get('state')
+    settings = smart_defaults  #the global defaults
+    settings.update(new_settings) #specific overriding defaults
     if state:
         return client.FHIRClient(state=state, save_func=_save_state)
     else:
@@ -52,6 +81,9 @@ def _med_name(prescription):
         return prescription.text.div
     return "Unnamed Medication(TM)"
 
+def _vitals_iterator(smart):
+    return iterentries('Observation?patient='+pat.resource.id+'&category=vital-signs&_format=json',smart)
+
 
 # views
 
@@ -60,31 +92,56 @@ def _med_name(prescription):
 def index():
     """ entry point for standalone launch - cannot get authorization to work.
     """
-    _reset()
-    smart = _get_smart()
     body = "<h1>Hello</h1>"
-    if smart.ready and smart.patient is not None:       # "ready" may be true but the access token may have expired, making smart.patient = None
-        name = smart.human_name(smart.patient.name[0] if smart.patient.name and len(smart.patient.name) > 0 else 'Unknown')
-
-        # generate simple body text
-        body += "<p>You are authorized and ready to make API requests for <em>{0}</em>.</p>".format(name)
-        pres = _get_prescriptions(smart)
-        if pres is not None:
-            body += "<p>{0} prescriptions: <ul><li>{1}</li></ul></p>".format("His" if 'male' == smart.patient.gender else "Her", '</li><li>'.join([_med_name(p) for p in pres]))
-        else:
-            body += "<p>(There are no prescriptions for {0})</p>".format("him" if 'male' == smart.patient.gender else "her")
-        body += """<p><a href="/logout">Change patient</a></p>"""
-    else:
-        auth_url = smart.authorize_url
-        if auth_url is not None:
-            logging.debug('Requesting Authorize with URL: '+auth_url)
-            body += """<p>Please <a href="{0}">authorize</a>.</p>""".format(auth_url)
-        else:
-            body += """<p>Running against a no-auth server, nothing to demo here. """
-        body += """<p><a href="/reset" style="font-size:small;">Reset</a></p>"""
+    body += "Not a stand alone app"
     return body
 
-@app.route('/after_token.html')
+@app.route('/launch.html')
+def launch():
+    """ The entry point for an emr launch
+    """
+    _reset()
+    settings = {
+              'api_base': request.args['iss'],
+              'launch_token': request.args['launch'],
+              'app_id': '9c4adaf6-5f54-4f33-8465-3ee770339d1d',
+              'redirect_uri': 'http://localhost:8000/fhir-app/after_token'
+              }
+    smart = _get_smart(settings)
+    return redirect(smart.authorize_url)
+
+@app.route('/launchdoc.html')
+def launchdoc():
+    _reset()
+    settings = {
+              'api_base': request.args['iss'],
+              'launch_token': request.args['launch'],
+              'app_id': 'e6b67cc5-200e-4264-b8ad-980b3e32abda',
+              'redirect_uri': 'http://localhost:8000/fhir-app/after_token2'
+              }
+    smart = _get_smart(settings)
+    return redirect(smart.authorize_url)
+
+@app.route('/fhir-app/after_token')
+@app.route('/fhir-app/after_token2')
+def callback():
+    """ OAuth2 callback interception.
+        Gets a token
+    """
+    logging.debug('In callback')
+    logging.debug(request.args)  #args are state and code
+    logging.debug('Request url:'+request.url)
+    smart = _get_smart()
+    logging.debug('After get smart')
+    try:
+        smart.handle_callback(request.url)
+    except Exception as e:
+        return """<h1>Authorization Error</h1><p>{0}</p><p><a href="/">Start over</a></p>""".format(e)
+    newurl = r'/' + request.url.rpartition('?')[0].rpartition(r'/')[2]
+    logging.debug('redirecting to: '+ newurl)
+    return redirect(newurl)
+
+@app.route('/after_token')
 def after_token():
     # this generates the app output display of a single patient Med list
     smart = _get_smart()
@@ -99,39 +156,22 @@ def after_token():
     body += """<p><a href="/logout">Change patient</a></p>"""
     return body
 
-@app.route('/launch.html')
-def launch():
-    """ The entry point for an emr launch
-    """
-    global smart_defaults
-    _reset()
-    smart_defaults = {
-              'api_base': request.args['iss'],
-              'launch_token': request.args['launch'],
-    ##              'app_id': '4d328327-b2af-4539-a683-99898974b4ee',  #app_id for public app
-              'app_id': '9c4adaf6-5f54-4f33-8465-3ee770339d1d', # app_id for confidential app
-              'redirect_uri': 'http://localhost:8000/fhir-app/'
-              }
+@app.route('/after_token2')
+def after_token2():
     smart = _get_smart()
-    return redirect(smart.authorize_url)
+    id = smart.patient.id
+    name = smart.patient.name
+    birthDate = smart.patient.birthDate
+    gender = smart.patient.gender
+    # get all the observation resource in the category 'vital-signs' for this patient in a list called vitals
+    vitals = [x[0].resource for x in iterentries('Observation?patient='+ id +'&category=vital-signs&_format=json',smart)]
+    body = smart.human_name(smart.patient.name[0] if smart.patient.name and len(smart.patient.name) > 0 else 'Unknown')
+    body += str(vitals[0].as_json())
+    return body
 
 
-@app.route('/fhir-app/')
-def callback():
-    """ OAuth2 callback interception.
-        Gets a token
-    """
-    logging.debug('In callback')
-    logging.debug(request.args)
-    logging.debug('Request url:'+request.url)
-    smart = _get_smart()
-    logging.debug('After get smart')
-    try:
-        smart.handle_callback(request.url)
-    except Exception as e:
-        return """<h1>Authorization Error</h1><p>{0}</p><p><a href="/">Start over</a></p>""".format(e)
 
-    return redirect('after_token.html')
+
 
 
 @app.route('/logout')
@@ -144,10 +184,6 @@ def logout():
 def reset():
     _reset()
     return redirect('/')
-
-@app.route('/bp')
-def bp_check():
-    return "bp check here"
 
 
 # start the app
